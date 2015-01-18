f93194e222ad3f284c89e61d67033f4b0166a692
Brian Chitester <brianchitester@gmail.com>
2015-01-18 17:09:46 -0500

    more Buffett, less todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,31 @@
 'use strict';
+ 
 
 angular.module('buffettchesneyApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-    $scope.addTodo = function () {
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
+    $scope.start = function () {
+		$.getJSON("../../songs.json", function(data) {
+	      //coin flip
+	      if (Math.floor(Math.random() * 2)){
+	      	var index = Math.floor(Math.random() * data.buffett.length);
+	      	console.log("buffett");
+	      	$scope.answer = "Buffett";
+	      	$(".song").html(data.buffett[index]);
+	      }
+	      else{
+	      	var index = Math.floor(Math.random() * data.chesney.length);
+	      	$scope.answer = "Chesney";
+	      	$(".song").html(data.chesney[index]);
+	      }
+      // data is a JavaScript object now. Handle it as such
+  	  });
+    };
+    $scope.choose = function () {
+    	if ($scope.answer == $scope.userAnswer){
+    		alert("correct!");
+    	}
+    };
+    $scope.setAnswer = function(answer) {
+    	$scope.userAnswer = answer;
     };
   });
