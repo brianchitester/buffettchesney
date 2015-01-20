1b28ad1290afddd62ca93d82142a44ebb2afe768
Brian Chitester <brianchitester@gmail.com>
2015-01-19 20:17:59 -0500

    finsihed version 0.1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,29 +3,47 @@
 
 angular.module('buffettchesneyApp')
   .controller('MainCtrl', function ($scope) {
+  	var coinFlip = function () {
+  		if (Math.floor(Math.random() * 2)){
+		    var index = Math.floor(Math.random() * $scope.data.buffett.length);
+		    console.log("buffett");
+		    $scope.answer = "Buffett";
+		    $(".song").html($scope.data.buffett[index]);
+	    }
+	    else{
+		    var index = Math.floor(Math.random() * $scope.data.chesney.length);
+		    $scope.answer = "Chesney";
+		    $(".song").html($scope.data.chesney[index]);
+	    }
+  	}
     $scope.start = function () {
 		$.getJSON("../../songs.json", function(data) {
-	      //coin flip
-	      if (Math.floor(Math.random() * 2)){
-	      	var index = Math.floor(Math.random() * data.buffett.length);
-	      	console.log("buffett");
-	      	$scope.answer = "Buffett";
-	      	$(".song").html(data.buffett[index]);
-	      }
-	      else{
-	      	var index = Math.floor(Math.random() * data.chesney.length);
-	      	$scope.answer = "Chesney";
-	      	$(".song").html(data.chesney[index]);
-	      }
-      // data is a JavaScript object now. Handle it as such
-  	  });
+			$scope.data = data;
+			coinFlip();
+			$(".start").fadeOut(400, function() {
+				$("#question").fadeIn(400);
+			});
+		});
+    };
+    $scope.next = function () {
+    	coinFlip();
+    	$("#answer").fadeOut(400, function() {
+    		$("#result").html("");
+    		$("#question").fadeIn(400);
+    	});
     };
     $scope.choose = function () {
     	if ($scope.answer == $scope.userAnswer){
-    		alert("correct!");
+    		$("#result").html("CORRECT!");
+    	}
+    	else {
+    		$("#result").html("WRONG!")
     	}
     };
     $scope.setAnswer = function(answer) {
     	$scope.userAnswer = answer;
+    	$("#question").fadeOut(400, function(){
+			$("#answer").fadeIn(400);
+		});
     };
-  });
+});
